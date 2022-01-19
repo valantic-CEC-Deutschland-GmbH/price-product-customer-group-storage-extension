d107744c571d23212b6e9d77f3cb27b1cf10d300
Dennis Blümer <d.bluemer@nexus-united.com>
2022-01-19 16:24:37 +0100

    Add missing namespace

--- src/ValanticSpryker/Zed/PriceProductCustomerGroupStorageExtension/Dependency/Plugin/PriceProductCustomerGroupStorageFilterPluginInterface.php
+++ src/ValanticSpryker/Zed/PriceProductCustomerGroupStorageExtension/Dependency/Plugin/PriceProductCustomerGroupStorageFilterPluginInterface.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+namespace ValanticSpryker\Zed\PriceProductCustomerGroupStorageExtension\Dependency\Plugin;
+
 interface PriceProductCustomerGroupStorageFilterPluginInterface
 {
     /**
